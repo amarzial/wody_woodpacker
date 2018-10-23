b8ecc80c7f5d7f38cffdd722a00299a55b8ac472
Alessandro Marzialetti <ale.marzialetti@gmail.com>
2018-10-23 11:56:13 +0200

    started aes

---
+++ include/aes.h
@@ -0,0 +1,14 @@
+#ifndef AES_H
+# define AES_H
+
+# include <stdint.h>
+
+// size expressed in 32 bit words
+# define BLOCK_SIZE 4
+# define KEY_SIZE 4
+# define AES_ROUNDS 10
+
+typedef uint32_t t_word;
+
+
+#endif
